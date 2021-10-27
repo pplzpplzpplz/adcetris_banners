3cc8614f09de4ea8e217d158d8fc27aaaa16bac3
pplzpplzpplz <moore.caleb@gmail.com>
2021-10-26 17:32:09 -0700

    same to W version 300x250

--- W-300x250/script.js
+++ W-300x250/script.js
@@ -54,6 +54,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // ***** GSAP *****
 
+    gsap.config({
+      force3D: false
+    });
+    
     var tl = gsap.timeline({
       repeat: -1
     });
